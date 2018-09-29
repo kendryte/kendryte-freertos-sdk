acc405d39d19336fb4c986c9193f5c5995953c49
Guo Hui <sunnycase@live.cn>
2018-09-29 15:33:12 +0800

    Remove duplicated license headers

--- lib/bsp/entry_user.c
+++ lib/bsp/entry_user.c
@@ -12,11 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Enable kernel-mode log API */
-#include <stdlib.h>
-
 #include <FreeRTOSConfig.h>
 #include <hal.h>
+#include <stdlib.h>
 #include <sysctl.h>
 #include "clint.h"
 #include "entry.h"
--- lib/bsp/interrupt.c
+++ lib/bsp/interrupt.c
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Enable kernel-mode log API */
 #include <stdint.h>
 #include <stdlib.h>
 #include <syslog.h>
